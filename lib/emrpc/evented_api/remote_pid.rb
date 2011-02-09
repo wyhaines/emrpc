631d9a33ea991faff7d357458aa0891fb2f0a1a4
Kirk Haines <wyhaines@gmail.com>
2011-02-08 22:17:47 -0700

    Cleaned up and elaborated on some comments.

--- lib/emrpc/evented_api/remote_pid.rb
+++ lib/emrpc/evented_api/remote_pid.rb
@@ -1,5 +1,8 @@
 module EMRPC  
-  # RemotePid is an interface for the actual remote pid.
+  # RemotePid is the interface through which one interacts with a remote Pid.
+  # It allows one to call methods on the remote Pid or kill the remote Pid,
+  # and it implements a useful #inspect method which will return information
+  # about the remote Pid.
   class RemotePid
     include Pid
     attr_accessor :_connection
@@ -11,20 +14,27 @@ module EMRPC
       @options     = options
     end
 
+    # As a shortcut for using #send, one can invoke arbitrary methods on
+    # an instance of RemotePid, which will proxy those method calls over
+    # to #send.
     def method_missing(*args)
       send(*args)
     end
 
+    # This is used to send a message (a method call)to a remote Pid. It is
+    # invoked by passing a symbol identifying the method to invoke, followed
+    # by the arguments to pass to the method.
     def send(*args)
       cmd = args.first
       if cmd == :kill
-        return if @killed
+        return if @killed # It's not undead; you can't kill it twice.
         @killed = true
         @_connection.close_connection_after_writing
       end
       @_connection.send_raw_message(args)
     end
-    
+
+    # Tell the remote Pid that it should die.
     def kill
       send(:kill)
     end
